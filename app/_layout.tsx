6d41b62efd5dfa2884857685d293b4491a1c34ab
Kayla Himmelberger <kaylahimmelberger@gmail.com>
2024-09-02 19:43:25 -0700

    add navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,40 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import Entypo from "@expo/vector-icons/Entypo";
+import { theme } from "../theme";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Grocery List" }} />
-    </Stack>
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorRed }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Grocery List",
+          tabBarIcon: ({ color, size }) => {
+            return <Entypo name="list" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => {
+            {
+              return <Entypo name="time-slot" size={size} color={color} />;
+            }
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="idea"
+        options={{
+          title: "Idea",
+          tabBarIcon: ({ color, size }) => {
+            return <Entypo name="light-bulb" size={size} color={color} />;
+          },
+        }}
+      />
+    </Tabs>
   );
 }
